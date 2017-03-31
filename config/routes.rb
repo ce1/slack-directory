48ba6ad659a2e7f774ae90632c12505afcf8058e
Christian Eggert <itschrstn@gmail.com>
2017-03-31 11:19:01 +0200

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :members, path_names: {sign_in: "login", sign_out: "logout"}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #root to: "devise/sessions#new"
+
+  devise_scope :member do
+    root to: "devise/sessions#new"
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
